ed177a6a912a22b82640a43a0c1cd7ae19bf0af8
dahoon06 <dahoon226@naver.com>
2024-08-22 10:17:10 +0900

    feat: 환경 변수 글로벌 관리

---
+++ client/config/processConfig.ts
@@ -0,0 +1,22 @@
+export const isProd = process.env.NODE_ENV === 'production';
+export const LOCAL_DOCKER_RUN = process.env.DOCKER_RUN === 'docker';
+export const PRODUCTION_HOST = process.env.NEXT_PUBLIC_PRODUCTION_API_URL;
+export const LOCAL_HOST = process.env.NEXT_PUBLIC_LOCAL_API_URL;
+
+export const BASE_URL = isProd ? PRODUCTION_HOST : LOCAL_HOST;
+
+/**
+ * =========================
+ * @description Next Auth 환경 변수
+ * =========================
+ */
+export const NEXT_AUTH_SECRET_KEY =  process.env.NEXTAUTH_SECRET as string;
+
+/**
+ * =========================
+ * @description 카카오 환경 변수
+ * =========================
+ */
+export const KAKAO_API_KEY = process.env.NEXT_PUBLIC_KAKAO_API_KEY as string;
+export const NEXT_AUTH_KAKAO_CLIENT_ID =  process.env.KAKAO_CLIENT_ID as string;
+export const NEXT_AUTH_KAKAO_SECRET_KEY = process.env.KAKAO_CLIENT_SECRET as string;
--- client/lib/axios/index.ts
+++ client/lib/axios/index.ts
@@ -1,13 +1,8 @@
 import axios, {AxiosInstance, AxiosRequestConfig, AxiosResponse, AxiosStatic,} from "axios";
+import {BASE_URL, LOCAL_DOCKER_RUN} from "@config/processConfig";
 
-const DOCKER_RUN = process.env.DOCKER_RUN === 'docker';
+const baseURL = LOCAL_DOCKER_RUN ? `http://server:4000/api` : `${BASE_URL}/api`;
 
-const BASE_URL =
-  process.env.NODE_ENV === "production"
-    ? process.env.NEXT_PUBLIC_PRODUCTION_API_URL
-    : process.env.NEXT_PUBLIC_LOCAL_API_URL;
-
-const baseURL = DOCKER_RUN ? `http://server:4000/api` : `${BASE_URL}/api`;
 const instance: AxiosInstance = axios.create({
   baseURL,
   headers: {
